fdd34f537b7aa9d2614b1fdceb27dbc6d37f2fc8
wakari.yasui <orzorzorz12345@gmail.com>
2021-03-13 20:19:42 +0000

    user login

---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,8 @@
+module TasksHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    !!current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 end
